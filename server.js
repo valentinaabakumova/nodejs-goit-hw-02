0c9112eef8c2a7ac0f886a887e9d2411a6754533
valentinaabakumova <abakumova.va13@gmail.com>
2022-12-24 16:49:12 +0100

    first

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const { PORT = 3000 } = process.env;
+const DB_HOST =
+  "mongodb+srv://valentina:5aMX7R4KOAEC7U4A@cluster0.gpimnpq.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+      console.log("Server running. Use our API on port: 3000");
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
