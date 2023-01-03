11cb8b372b4002c0d0fccefaee6eaa2c7cec011b
valentinaabakumova <abakumova.va13@gmail.com>
2023-01-03 10:23:29 +0100

    2

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,6 +1,6 @@
-const Contact = require('../../models/contacts');
+const Contact = require("../../models/contacts");
 
-const { contactSchema } = require('../../schemas/contacts');
+const { contactSchema } = require("../../schemas/contacts");
 
 const addContact = async (req, res, next) => {
   const { _id } = req.user;
@@ -8,7 +8,7 @@ const addContact = async (req, res, next) => {
   try {
     const { error } = contactSchema.validate(req.body);
     if (error) {
-      return res.status(400).json({ message: 'missing required name field' });
+      return res.status(400).json({ message: "missing required name field" });
     }
     const result = await Contact.create({ ...req.body, owner: _id });
     res.status(201).json(result);
