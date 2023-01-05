6356b8818acec489d7d7a1597e7369b60f1bc571
valentinaabakumova <abakumova.va13@gmail.com>
2023-01-05 14:05:39 +0100

    first

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,15 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const ctrl = require("../../controllers/users");
-const auth = require("../../middlewares/auth");
-const upload = require("../../middlewares/upload");
+const ctrl = require('../../controllers/users');
+const auth = require('../../middlewares/auth');
+const upload = require('../../middlewares/upload');
 
-router.get("/current", auth, ctrl.getCurrent);
+router.get('/current', auth, ctrl.getCurrent);
 
-router.patch("/avatars", auth, upload.single("avatar"), ctrl.updateAvatar);
+router.patch('/avatars', auth, upload.single('avatar'), ctrl.updateAvatar);
+
+router.get('/verify/:verificationToken', ctrl.verify);
+
+router.post('/verify', ctrl.resendEmail);
 
 module.exports = router;
